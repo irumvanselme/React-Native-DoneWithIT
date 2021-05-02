cb224d64eb01bf381a973a0af087ac72fcaa266e
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-02 08:27:05 +0200

    Finish the welcome screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, StatusBar as TopBar } from "react-native";
+import ViewImageScreen from "./screens/ViewImageScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <WelcomeScreen />
+            <ViewImageScreen />
         </View>
     );
 }
--- screens/ViewImageScreen.js
+++ screens/ViewImageScreen.js
@@ -1,12 +1,47 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 function ViewImageScreen() {
     return (
-        <View>
-            <Text>This is the View Image Screen</Text>
+        <View style={styles.container}>
+            <View
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    paddingTop: 20,
+                    paddingHorizontal: 20,
+                }}
+            >
+                <View
+                    style={{ width: 50, height: 50, backgroundColor: "tomato" }}
+                />
+                <View
+                    style={{
+                        width: 50,
+                        height: 50,
+                        backgroundColor: "dodgerblue",
+                    }}
+                />
+            </View>
+            <Image
+                resizeMode="contain"
+                style={styles.image}
+                source={require("../assets/chair.jpg")}
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black",
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
 
 export default ViewImageScreen;
