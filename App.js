3f60499602441126a7921f3965cfc8bae0ba3f05
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-02 12:23:29 +0200

    Made the switch

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch } from "react-native";
 import FormControl from "./components/FormControl";
 import Screen from "./layouts/Screen";
 
 export default function App() {
+    const [value, setValue] = useState(false);
     return (
         <Screen>
-            <FormControl />
+            <Switch
+                value={value}
+                onValueChange={(newValue) => setValue(newValue)}
+            />
         </Screen>
     );
 }
