6c177505e5a3883d31673978d677133b6985cd50
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-07 07:06:19 +0200

    Lesson One Done

--- App.js
+++ App.js
@@ -1,38 +1,11 @@
 import React from "react";
-import { Button, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import Screen from "./my-code/layouts/Screen";
 import { NavigationContainer } from "@react-navigation/native";
-
-const Tweets = ({ navigation }) => (
-    <Screen>
-        <Text>Here is the Tweets</Text>
-        <Button
-            title="View Tweet"
-            onPress={() => navigation.navigate("TweetDetails")}
-        />
-    </Screen>
-);
-
-const TweetDetails = () => (
-    <Screen>
-        <Text>Here is the Tweet Details</Text>
-    </Screen>
-);
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-    <Stack.Navigator>
-        <Stack.Screen name="Tweets" component={Tweets} />
-        <Stack.Screen name="TweetDetails" component={TweetDetails} />
-    </Stack.Navigator>
-);
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
     return (
         <NavigationContainer>
-            <StackNavigator />
+            <AuthNavigator />
         </NavigationContainer>
     );
 }
